559516e537f435fd7a15029f4eb8080892da6d6e
dane <danafetterman@DANAs-MacBook-Air.local>
2016-12-06 22:56:54 -0500

    added some notes

--- pwgenmd5.sh
+++ pwgenmd5.sh
@@ -3,9 +3,6 @@
 while read pw hostname; do
 #  echo $pw
   hierakey=$(echo "rootpw::"$hostname": >")
-#  sha512hashed="$(echo -n $pw | sha512sum | cut -d " " -f 1)"
-#  echo $sha512hashed
-#  eyamlencrypt=$(eyaml encrypt -s $sha512hashed | awk '{print $2}')
 
   md5hashed="$(openssl passwd -1 -salt b9490e92cb2c65d5f890a20a47f2bbd7 $pw)"    #I do not use this salt in real life, or md5
 #  echo "View your md5 hash:"
